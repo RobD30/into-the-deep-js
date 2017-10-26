e90df578ef23c8c06c0a5ea6d9a9d0c8f241902f
Robert Dickerson <rdickeriii@gmail.com>
2017-10-26 14:53:08 -0700

    Refactor: explicit binding
    end_of_line

--- scopes_scratch.js
+++ scopes_scratch.js
@@ -3,9 +3,7 @@ function foo() {
 }
 
 var bar = 'bar1';
-var o2 = {bar: 'bar2', foo: foo};
-var o3 = {bar: 'bar3', foo: foo};
+var obj = {bar: 'bar2'};
 
 foo();
-o2.foo();
-o3.foo();
+foo.call(obj);
