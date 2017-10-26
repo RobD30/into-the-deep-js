a8d77fe882e37cdd97bc789e7d61f2d85280f6ec
Robert Dickerson <rdickeriii@gmail.com>
2017-10-25 18:04:58 -0700

    Refactor: let wont hoist like that
    end_of_line

--- scopes_scratch.js
+++ scopes_scratch.js
@@ -1,12 +1,8 @@
-a(1);
+function foo(bar) {
+    if (bar) {
+        console.log(baz);
+        let baz = bar;
+    }
+}
 
-function a(foo) {
-    if (foo > 20) return foo;
-    returnb(foo+2);
-}
-function b(foo) {
-    return c(foo) + 1;
-}
-function c(foo) {
-    return a(foo*2);
-}
+foo('bar');
