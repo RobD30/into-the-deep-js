c0f1e6e76c6bb29e6e1545b8f3cbd3fb2b8cf1d8
Robert Dickerson <rdickeriii@gmail.com>
2017-10-28 16:44:55 -0700

    Refactor: primitive types
    end_of_line

--- scopes_scratch.js
+++ scopes_scratch.js
@@ -1,19 +1,6 @@
-class Foo {
-    this.me = who;
-}
-Foo.prototype.identify = function () {
-    return 'I am ' + this.me;
-};
-
-var Bar = Object.create(Foo.prototype);
-Bar.init = function (who) {
-    Foo.call(this, who);
-};
-
-Bar.speak = function() {
-    alert('Hello, ' + this.indentify() + '.');
-};
-
-var b1 = Object.create(Bar);
-Bar.init('b1');
-b1.speak();
+typeof foo;
+typeof 'foo';
+typeof 123;
+typeof true;
+typeof {a:1};
+typeof function() {alert(e);};
