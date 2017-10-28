bf7e76cf4e99c563263ec1c46b36242b21fbb9da
Robert Dickerson <rdickeriii@gmail.com>
2017-10-28 10:55:38 -0700

    Refactor: prototypes
    end_of_line

--- scopes_scratch.js
+++ scopes_scratch.js
@@ -1,9 +1,19 @@
-function foo() {
-    console.log(this.bar);
+function foo(who) {
+    this.me = who;
 }
 
-var bar = 'bar1';
-var obj = {bar: 'bar2'};
+Foo.prototype.identify = function() {
+    return 'I am ' + this.me;
+};
 
-foo();
-foo.call(obj);
+var a1 = new Foo('a1');
+var a2 = new Foo('a2');
+
+a2.speak = function() {
+    alert('Hello, ' + this.identify() + '.');
+};
+
+a1.constructor === Foo;
+a1.constructor === a2.constructor;
+a1.__proto__ === Foo.prototype;
+a1.__proto__ === a2.__proto__;
