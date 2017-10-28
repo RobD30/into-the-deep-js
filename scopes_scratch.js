a1d19f9a08603acba846958e8a46b7b3092b36cf
Robert Dickerson <rdickeriii@gmail.com>
2017-10-28 12:51:04 -0700

    Refactor: super
    end_of_line

--- scopes_scratch.js
+++ scopes_scratch.js
@@ -1,23 +1,20 @@
-function foo(who) {
-    this.me = who;
+class Foo {
+    constructor(who) {
+        this.me = who;
+    }
+    identify() {
+        return 'I am ' + this.me;
+    }
 }
 
-Foo.prototype.identify = function() {
-    return 'I am ' + this.me;
-};
-
-function Bar(who) {
-    Foo.call(this,who);
+class Bar extends Foo {
+    identify() {
+        alert('Hello, ' +super.identify() + '.');
+    }
 }
-
-Bar.prototyp = Object.create(Foo.prototype);
-
-Bar.prototype.speak = function() {
-    alert('Hello, ' + this.identify() + '.');
-};
 
 var b1 = new Bar('b1');
 var b2 = new Bar('b2');
 
-b1.speak();
-b2.speak();
+b1 .identify();
+b2.identify();
